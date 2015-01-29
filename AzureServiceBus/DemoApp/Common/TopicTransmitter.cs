9d3c0b71bb496bc1f6a794a745f2908389e1d50f
Valentine Radchuk <zaknafeyn@gmail.com>
2015-01-29 18:23:28 +0200

    Add topic transmitter and remove redundant Transmitter

---
+++ AzureServiceBus/DemoApp/Common/TopicTransmitter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Common
+{
+    public abstract class TopicTransmitter<TClient> : BaseTransmitter where TClient : MessageClientEntity
+    {
+        protected TClient _topicClient;
+
+        protected TopicTransmitter(string topicName, string connectionString) : base(connectionString)
+        {
+            TopicName = topicName;
+        }
+
+        public void Configure()
+        {
+            ConfigureTopicSettings();
+            _topicClient = CreateTopicClient();
+        }
+
+        public string TopicName { get; protected set; }
+
+        protected virtual NamespaceManager ConfigureTopicSettings()
+        {
+            // Configure Queue Settings
+            var topicDescription = new TopicDescription(TopicName)
+            {
+                MaxSizeInMegabytes = 5120,
+                DefaultMessageTimeToLive = new TimeSpan(0, 10, 0)
+            };
+
+            // Create a new Queue with custom settings
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(_connectionString);
+
+            if (!namespaceManager.TopicExists(TopicName))
+            {
+                namespaceManager.CreateTopic(topicDescription);
+            }
+
+            return namespaceManager;
+        }
+
+        protected abstract TClient CreateTopicClient();
+
+    }
+}
